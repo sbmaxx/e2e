6ebeb2a9618bc81801365795b67602776775b41d
sbmaxx <sbmaxx@gmail.com>
2014-08-23 12:15:54 +0400

    messages

--- casperjs/index.js
+++ casperjs/index.js
@@ -5,24 +5,25 @@ casper.test.begin('yandex.images index', function(test) {
     });
 
     casper.then(function() {
-        test.assertTitle('Яндекс.Картинки: поиск изображений в интернете');
+
+        test.assertTitle('Яндекс.Картинки: поиск изображений в интернете', 'should open');
 
         this.fill('.search', {
             text: 'bmw'
         }, false);
 
-        test.assertField('text', 'bmw');
+        test.assertField('text', 'bmw', 'should fill input');
 
         this.click('.search .button');
 
         this.wait(1000, function() {
-            test.assertTitleMatches(/bmw: \d+ тыс изображений найдено в Яндекс.Картинках/);
+            test.assertTitleMatches(/bmw: \d+ тыс изображений найдено в Яндекс.Картинках/, 'should do search');
         });
 
         this.scrollToBottom();
 
         this.wait(1000, function() {
-            test.assertVisible('.more_direction_next');
+            test.assertVisible('.more_direction_next', 'more button should be visible after scroll');
         });
 
     });
