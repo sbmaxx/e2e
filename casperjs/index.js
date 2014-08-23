52d5a213a220952d18d2f465039cc31f0b2527b4
sbmaxx <sbmaxx@gmail.com>
2014-08-23 12:13:48 +0400

    one start

--- casperjs/index.js
+++ casperjs/index.js
@@ -3,27 +3,25 @@ casper.test.begin('yandex.images index', function(test) {
     casper.start('http://yandex.ru/images', function(response) {
         casper.viewport(1366, 900);
         test.assertTitle('Яндекс.Картинки: поиск изображений в интернете');
+
         this.fill('.search', {
             text: 'bmw'
         }, false);
-        test.assertField('text', 'bmw');
-    });
 
-    casper.then(function() {
+        test.assertField('text', 'bmw');
+
         this.click('.search .button');
+
         this.wait(1000, function() {
             test.assertTitleMatches(/bmw: \d+ тыс изображений найдено в Яндекс.Картинках/);
         });
-    });
 
-    casper.then(function() {
         this.scrollToBottom();
-    });
 
-    casper.then(function() {
         this.wait(1000, function() {
             test.assertVisible('.more_direction_next');
         });
+
     });
 
     casper.run(function() {
