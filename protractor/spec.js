4fad28eb52a620529f971753c04e64c76c68e7f6
Roman sbmaxx Rozhdestvenskiy <sbmaxx@yandex-team.ru>
2014-08-24 13:38:10 +0400

    more tests

--- protractor/spec.js
+++ protractor/spec.js
@@ -13,11 +13,10 @@
 
 describe('yandex.images index', function() {
 
-    browser.driver.manage().window().setSize(1300, 900);
-
     browser.get('http://yandex.ru/images');
 
     it('should open', function() {
+        browser.driver.manage().window().setSize(1000, 900);
         expect(browser.getTitle()).toEqual('Яндекс.Картинки: поиск изображений в интернете');
     });
 
@@ -35,28 +34,28 @@ describe('yandex.images index', function() {
     });
 
     it('loader on scroll', function() {
-        browser.executeScript('window.scrollTo(0,document.documentElement.scrollHeight);').then(function() {
-            expect(browser.isElementPresent($('.more_direction_next .spinner'))).toEqual(true);
-        });
-        browser.sleep(500);
-        browser.executeScript('window.scrollTo(0,document.documentElement.scrollHeight);').then(function() {
-            expect(browser.isElementPresent($('.more_direction_next .spinner'))).toEqual(true);
-        });
-        browser.sleep(500);
-        browser.executeScript('window.scrollTo(0,document.documentElement.scrollHeight);').then(function() {
-            expect(browser.isElementPresent($('.more_direction_next .spinner'))).toEqual(true);
-        });
-        browser.sleep(500);
-        browser.executeScript('window.scrollTo(0,document.documentElement.scrollHeight);').then(function() {
-            expect(browser.isElementPresent($('.more_direction_next .spinner'))).toEqual(true);
-        });
-        browser.sleep(500);
+            browser.executeScript('window.scrollTo(0,document.documentElement.scrollHeight);').then(function() {
+                expect(browser.isElementPresent($('.more_direction_next .spinner'))).toEqual(true);
+            });
+            browser.sleep(500);
+            browser.executeScript('window.scrollTo(0,document.documentElement.scrollHeight);').then(function() {
+                expect(browser.isElementPresent($('.more_direction_next .spinner'))).toEqual(true);
+            });
+            browser.sleep(500);
+            browser.executeScript('window.scrollTo(0,document.documentElement.scrollHeight);').then(function() {
+                expect(browser.isElementPresent($('.more_direction_next .spinner'))).toEqual(true);
+            });
+            browser.sleep(500);
+            browser.executeScript('window.scrollTo(0,document.documentElement.scrollHeight);').then(function() {
+                expect(browser.isElementPresent($('.more_direction_next .spinner'))).toEqual(true);
+            });
+            browser.sleep(500);
     });
 
     // not correct test but at least..
     it('more button should be visible after scroll', function() {
         var button = $('.more_direction_next');
-        expect(button.getAttribute('class')).not.toMatch('more__button_hidden_yes');
+        expect(button.isDisplayed()).toEqual(true);
     });
 
     it('should fill input with audi', function() {
@@ -72,9 +71,36 @@ describe('yandex.images index', function() {
         browser.sleep(1000);
         expect(browser.getTitle()).toMatch(/audi: \d+ тыс изображений найдено в Яндекс.Картинках/);
         expect($('.pane').isDisplayed()).toEqual(false);
+        browser.driver.manage().window().setSize(1500, 900);
+        expect($('.pane').isDisplayed()).toEqual(true);
     });
 
-    browser.driver.manage().window().setSize(1500, 900);
+    it('should go to index', function() {
+        $('.search .service__url').click();
+        browser.sleep(1000);
+        expect(browser.getTitle()).toEqual('Яндекс.Картинки: поиск изображений в интернете');
+        expect(browser.isElementPresent($('.b-page_type_index'))).toEqual(true);
+    });
+
+
+    it('should back to search', function() {
+        browser.driver.navigate().back();
+        browser.sleep(1000);
+        expect(browser.getTitle()).toMatch(/audi: \d+ тыс изображений найдено в Яндекс.Картинках/);
+    });
+
+    it('have some unselected items', function() {
+        var input = $('.search .input__control');
+        input.clear();
+        input.sendKeys('пластиковые окна');
+        var button = $('.search .button');
+        button.click();
+        browser.sleep(1000);
+        var serpItem = $('.serp-item_pos_3');
+        browser.driver.actions().mouseMove(serpItem).mouseMove(serpItem).perform();
+        browser.sleep(1000);
+        expect(browser.isElementPresent($('.serp-item_pos_3.serp-item_hovered_yes'))).toEqual(true);
+    });
 
 });
 
