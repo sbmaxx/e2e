c0a7b04bd2e547f7695bb3c7cb1bb385b621eac5
sbmaxx <sbmaxx@gmail.com>
2014-08-25 08:57:16 +0400

    comments to casperjs test

--- casperjs/index.js
+++ casperjs/index.js
@@ -9,6 +9,7 @@ casper.test.begin('yandex.images index', function(test) {
 
         test.assertTitle('Яндекс.Картинки: поиск изображений в интернете', 'should be index page');
 
+        // fill form without submit
         this.fill('.search', {
             text: 'bmw'
         }, false);
@@ -18,6 +19,7 @@ casper.test.begin('yandex.images index', function(test) {
         this.click('.search .button');
     });
 
+    // wait while the page is loading
     casper.waitWhileVisible('.fade', function() {
         test.assertTitleMatches(/bmw: \d+ тыс изображений найдено в Яндекс.Картинках/, 'should do search for "bmw"');
         this.scrollToBottom();
@@ -25,57 +27,71 @@ casper.test.begin('yandex.images index', function(test) {
 
     // do a lot of scroll like user does
     for(var i = 0; i < 4; i++) {
+        // check while the spinner is visible
         casper.waitWhileVisible('.more_direction_next .spinner', function() {
             test.pass('loader on scroll');
+            // do another scorll
             this.scrollToBottom();
         });
     }
 
+    // whait for button became visible
     casper.waitUntilVisible('.more_direction_next .button', function() {
         test.pass('more button should be visible after scroll');
 
+        // fill search form with another text without submit
         this.fill('.search', {
             text: 'audi'
         }, false);
 
         test.assertField('text', 'audi', 'should fill input with "audi" query');
 
+        // manual submit the form
         this.click('.search .button');
     });
 
+    // wait while the page is loading
     casper.waitWhileVisible('.fade', function() {
         test.assertTitleMatches(/audi: \d+ тыс изображений найдено в Яндекс.Картинках/, 'should do search for "audi"');
+        // click to service label
         this.click('.search .service__url');
     });
 
+    // wait until b-page change mod
     casper.waitUntilVisible('.b-page_type_index', function() {
         test.assertTitle('Яндекс.Картинки: поиск изображений в интернете', 'should be index after click to service-label');
     });
 
+    // do browser navigation back and wait until b-page change mod
     casper.back().waitUntilVisible('.b-page_type_search', function() {
         test.assertTitleMatches(/audi: \d+ тыс изображений найдено в Яндекс.Картинках/, 'should back to "audi" query');
+        // for this viewport we should not see the pane or any selected serp-items
         test.assertNotVisible('.pane', 'pane should not be visible on this viewport');
         test.assertDoesntExist('.serp-item_selected_yes', 'no image should be selected');
         casper.viewport(1500, 900);
     });
 
+    // wait for viewport changed
     casper.then(function() {
         test.assertVisible('.pane', 'pane should be visible');
         test.assertExists('.serp-item_selected_yes', 'image should be selected');
         test.assertExists('.serp-item_selected_yes.serp-item_pos_0', 'first image should be selected');
 
         // this can through warning, because we have prevent default on forms
+        // fill && submit
         this.fill('.search', {
             text: 'пластиковые окна'
         }, true);
     });
 
+    // wait for new searchdata
     casper.waitWhileVisible('.fade').then(function() {
         // we really need two mouse move for now :/
         this.mouse.move('.serp-item_pos_3');
         this.mouse.move('.serp-item_pos_3');
     });
 
+    // hovered_yes run via setTimeout, so wait for it
     casper.waitUntilVisible('.serp-item_hovered_yes', function() {
         test.pass('item should be hovered');
         test.assertVisible('.serp-item_pos_3 .serp-item__snippet', 'snippet should be visible');
