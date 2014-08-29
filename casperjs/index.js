dda777a331be906579e06d559488c2658e824b92
sbmaxx <sbmaxx@gmail.com>
2014-08-29 09:09:26 +0400

    cookie example

--- casperjs/index.js
+++ casperjs/index.js
@@ -1,5 +1,11 @@
 require("utils").dump(casper.cli.options);
 
+phantom.addCookie({
+  'name': 'name',
+  'value': 'value',
+  'domain': '.yandex.ru'
+});
+
 casper.test.begin('yandex.images index', function(test) {
 
     // casper.userAgent('Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36');
